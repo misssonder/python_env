e06e7b21ab958d8c17796f91f8ebc959662c17ce
misssonder <819643718@qq.com>
2019-11-13 19:18:06 +0800

    增加了书籍详情页面

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def load_user(student_no):
 
 
 @app.route('/')
-@login_required
+# @login_required
 def index():
     return render_template('index.html', name=name, movies=movies, res=res)
 
@@ -147,7 +147,7 @@ def logout():
 
 
 @app.route('/searchbook/', methods=['GET', 'POST'])
-@login_required
+# @login_required
 def searchbook():
     if request.method == 'POST':
         bookname = request.form.get('bookname')
@@ -175,6 +175,10 @@ def show():
             return '请输入查询数据！'
     else:
         return 'Please Input Date！'
+@app.route("/<ISBN>/information/")
+def show_information(ISBN):
+    book = Book.query.get(str(ISBN))
+    return render_template("show_information.html",book=book)
 
 
 app.config['DEBUG'] = True
